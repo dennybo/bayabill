c1cbf13ee08d219d37f12e79cc7c4123353dbeac
Iman Vafadarnezhad <iman.v846@gmail.com>
2017-02-25 22:11:37 +0330

    Added signup and signin templates

--- account/views.py
+++ account/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import SignupForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            redirect(reverse_lazy('account:signin'))
+    else:
+        form = SignupForm()
+    return render(request, 'account/signup.html', {
+        'form': form
+    })
