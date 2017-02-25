7aff0d3d5950db35bccbc4a8521c2446a5b38779
Iman Vafadarnezhad <iman.v846@gmail.com>
2017-02-25 19:26:52 +0330

    Added mysql database configuration

--- bayabill/settings.py
+++ bayabill/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'bayabill.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'bayabill',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
