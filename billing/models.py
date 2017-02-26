b6e18c40358573cab2b16c3ca198a9ae965310c4
Iman Vafadarnezhad <iman.v846@gmail.com>
2017-02-26 23:57:07 +0330

    Added language, currency, country and store model

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,147 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Language(models.Model):
+    id = models.AutoField(
+        verbose_name=_('ID'),
+        primary_key=True,
+    )
+    iso_code = models.CharField(
+        verbose_name=_('ISO Code'),
+        max_length=2,
+        null=False,
+        blank=False,
+        unique=True,
+    )
+    title = models.CharField(
+        verbose_name=_('Title'),
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+    is_active = models.BooleanField(
+        verbose_name=_('Is Active'),
+        null=False,
+        blank=False,
+        default=True,
+    )
+
+
+class Currency(models.Model):
+    id = models.AutoField(
+        verbose_name=_('ID'),
+        primary_key=True,
+    )
+    iso_code = models.CharField(
+        verbose_name=_('ISO Code'),
+        max_length=3,
+        null=False,
+        blank=False,
+        unique=True,
+    )
+    title = models.CharField(
+        verbose_name=_('Title'),
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+    prefix = models.CharField(
+        verbose_name=_('Prefix'),
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    suffix = models.CharField(
+        verbose_name=_('Suffix'),
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    rate = models.DecimalField(
+        verbose_name=_('Conversion Rate'),
+        null=False,
+        blank=False,
+        default=1,
+    )
+    is_active = models.BooleanField(
+        verbose_name=_('Is Active'),
+        null=False,
+        blank=False,
+        default=True,
+    )
+
+
+class Country(models.Model):
+    id = models.AutoField(
+        verbose_name=_('ID'),
+        primary_key=True,
+    )
+    iso_code = models.CharField(
+        verbose_name=_('ISO Code'),
+        max_length=2,
+        null=False,
+        blank=False,
+        unique=True,
+    )
+    title = models.CharField(
+        verbose_name=_('Title'),
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+    default_language = models.ForeignKey(
+        Language,
+        related_name='countries',
+        null=True,
+        blank=True,
+    )
+    default_currency = models.ForeignKey(
+        Currency,
+        related_name='countries',
+        null=True,
+        blank=True,
+    )
+    is_active = models.BooleanField(
+        verbose_name=_('Is Active'),
+        null=False,
+        blank=False,
+        default=True,
+    )
+
+
+class Store(models.Model):
+    id = models.AutoField(
+        verbose_name=_('ID'),
+        primary_key=True,
+    )
+    title = models.CharField(
+        verbose_name=_('Title'),
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+    url = models.URLField(
+        verbose_name=_('Store Url'),
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+    ssl_enabled = models.BooleanField(
+        verbose_name=_('SSL Enabled'),
+        null=False,
+        blank=False,
+        default=True,
+    )
+    is_default = models.BooleanField(
+        verbose_name=_('Is Default'),
+        null=False,
+        blank=False,
+        default=False,
+    )
+    is_active = models.BooleanField(
+        verbose_name=_('Is Active'),
+        null=False,
+        blank=False,
+        default=True,
+    )
