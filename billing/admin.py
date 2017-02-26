da8cf9e4b01fe3162a131c055f14138ade899d15
Iman Vafadarnezhad <iman.v846@gmail.com>
2017-02-27 00:26:32 +0330

    Added currency, language, country and store model to admin site

--- billing/admin.py
+++ billing/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Language, Currency, Country, Store
+
+admin.site.register(Language)
+admin.site.register(Currency)
+admin.site.register(Country)
+admin.site.register(Store)
