b0226a476cac249f7d4d3bcef8b782b5e5788121
Iman Vafadarnezhad <iman.v846@gmail.com>
2017-02-27 16:28:48 +0330

    Aded ProductPricing model

--- billing/models.py
+++ billing/models.py
@@ -291,6 +291,11 @@ class CategoryLang(models.Model):
 
 
 class Product(models.Model):
+    Payment_Types = (
+        ('free', _('Free')),
+        ('onetime', _('One-Time')),
+        ('recurring', _('Recurring')),
+    )
     id = models.AutoField(
         verbose_name=_('ID'),
         primary_key=True,
@@ -304,6 +309,13 @@ class Product(models.Model):
     category = models.ForeignKey(
         Category,
         related_name='products',
+        null=False,
+        blank=False,
+    )
+    payment_types = models.CharField(
+        verbose_name=_('Payment Type'),
+        max_length=16,
+        choices=Payment_Types,
         null=False,
         blank=False,
     )
@@ -383,3 +395,92 @@ class ProductLang(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ProductPricing(models.Model):
+    id = models.AutoField(
+        verbose_name=_('ID'),
+        primary_key=True,
+    )
+    product = models.ForeignKey(
+        Product,
+        related_name='pricing',
+        null=False,
+        blank=False,
+    )
+    currency = models.ForeignKey(
+        Currency,
+        related_name='pricing',
+        null=False,
+        blank=False,
+    )
+    onetime = models.DecimalField(
+        verbose_name=_('One Time'),
+        max_digits=10,
+        decimal_places=4,
+        null=True,
+        blank=True,
+    )
+    hourly = models.DecimalField(
+        verbose_name=_('Hourly'),
+        max_digits=10,
+        decimal_places=4,
+        null=True,
+        blank=True,
+    )
+    Daily = models.DecimalField(
+        verbose_name=_('Daily'),
+        max_digits=10,
+        decimal_places=4,
+        null=True,
+        blank=True,
+    )
+    weekly = models.DecimalField(
+        verbose_name=_('Weekly'),
+        max_digits=10,
+        decimal_places=4,
+        null=True,
+        blank=True,
+    )
+    monthly = models.DecimalField(
+        verbose_name=_('Monthly'),
+        max_digits=10,
+        decimal_places=4,
+        null=True,
+        blank=True,
+    )
+    quarterly = models.DecimalField(
+        verbose_name=_('Quarterly'),
+        max_digits=10,
+        decimal_places=4,
+        null=True,
+        blank=True,
+    )
+    semi_annually = models.DecimalField(
+        verbose_name=_('Semi-Annually'),
+        max_digits=10,
+        decimal_places=4,
+        null=True,
+        blank=True,
+    )
+    annually = models.DecimalField(
+        verbose_name=_('Annually'),
+        max_digits=10,
+        decimal_places=4,
+        null=True,
+        blank=True,
+    )
+    biennially = models.DecimalField(
+        verbose_name=_('Biennially'),
+        max_digits=10,
+        decimal_places=4,
+        null=True,
+        blank=True,
+    )
+    triennially = models.DecimalField(
+        verbose_name=_('Triennially'),
+        max_digits=10,
+        decimal_places=4,
+        null=True,
+        blank=True,
+    )
