a70286047ad94a4d44835eb93f60fecddd8110af
Iman Vafadarnezhad <iman.v846@gmail.com>
2017-03-06 17:28:19 +0330

    Improved user model and multi-site support added

--- account/migrations/0001_initial.py
+++ account/migrations/0001_initial.py
@@ -7,6 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('sites', '0001_initial'),
         ('auth', '0006_require_contenttypes_0002'),
     ]
 
@@ -18,16 +19,22 @@ class Migration(migrations.Migration):
                 ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
                 ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
                 ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True)),
-                ('email', models.EmailField(unique=True, max_length=255, verbose_name='Email Address', db_index=True)),
-                ('first_name', models.CharField(max_length=32, null=True, verbose_name='First Name', blank=True)),
-                ('last_name', models.CharField(max_length=32, null=True, verbose_name='Last Name', blank=True)),
+                ('email', models.EmailField(unique=True, max_length=255, verbose_name='Email Address')),
+                ('first_name', models.CharField(max_length=32, null=True, verbose_name='First Name')),
+                ('last_name', models.CharField(max_length=32, null=True, verbose_name='Last Name')),
                 ('is_active', models.BooleanField(default=True, db_index=True, verbose_name='Is Active')),
                 ('create_date', models.DateTimeField(auto_now_add=True, verbose_name='Create Date')),
+                ('update_date', models.DateTimeField(auto_now=True, verbose_name='Update Date', null=True)),
                 ('groups', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups')),
+                ('site', models.ForeignKey(related_name='users', to='sites.Site')),
                 ('user_permissions', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions')),
             ],
             options={
+                'get_latest_by': 'create_date',
+                'verbose_name': 'User',
+                'verbose_name_plural': 'Users',
                 'db_table': 'auth_user',
+                'default_related_name': 'users',
             },
         ),
     ]
--- account/models.py
+++ account/models.py
@@ -1,70 +1,87 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.sites.models import Site
 from django.db import models
-from django.dispatch import receiver
-from django.utils import timezone
+from django.db.models import fields
 from django.utils.translation import ugettext_lazy as _
 
 
 class UserManager(BaseUserManager):
 
-    def __create_user(self, email, password=None):
+    def __create_user(self, site, email, password=None):
         if not email:
-            raise ValueError('User must have an email address.')
+            raise ValueError(_('User must have an email address.'))
+        try:
+            site = Site.objects.get(id=site)
+        except Site.DoesNotExist:
+            raise ValueError(_('Site ID is invalid.'))
         user = self.model(
+            site=site,
             email=self.normalize_email(email),
         )
         user.set_password(password)
         return user
 
-    def create_user(self, email, password=None):
-        user = self.__create_user(email, password)
+    def create_user(self, site, email, password=None):
+        user = self.__create_user(site, email, password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
-        user = self.__create_user(email, password)
+    def create_superuser(self, site, email, password):
+        user = self.__create_user(site, email, password)
         user.is_superuser = True
         user.save(using=self._db)
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    id = models.AutoField(
+    id = fields.AutoField(
         verbose_name=_('ID'),
         primary_key=True,
     )
-    email = models.EmailField(
+    site = models.ForeignKey(
+        Site,
+        related_name='users',
+        null=False,
+        blank=False,
+        db_index=True,
+    )
+    email = fields.EmailField(
         verbose_name=_('Email Address'),
         max_length=255,
         null=False,
         blank=False,
         unique=True,
-        db_index=True,
     )
-    first_name = models.CharField(
+    first_name = fields.CharField(
         verbose_name=_('First Name'),
         max_length=32,
         null=True,
-        blank=True,
+        blank=False,
     )
-    last_name = models.CharField(
+    last_name = fields.CharField(
         verbose_name=_('Last Name'),
         max_length=32,
         null=True,
-        blank=True,
+        blank=False,
     )
-    is_active = models.BooleanField(
+    is_active = fields.BooleanField(
         verbose_name=_('Is Active'),
         null=False,
         blank=False,
         default=True,
         db_index=True,
     )
-    create_date = models.DateTimeField(
+    create_date = fields.DateTimeField(
         verbose_name=_('Create Date'),
         null=False,
-        blank=False,
+        blank=True,
         auto_now_add=True,
+    )
+    update_date = fields.DateTimeField(
+        verbose_name=_('Update Date'),
+        null=True,
+        blank=True,
+        auto_now=True,
     )
 
     @property
@@ -72,15 +89,19 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.is_superuser
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['site']
 
     objects = UserManager()
 
     class Meta:
         db_table = 'auth_user'
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+        get_latest_by = 'create_date'
+        default_related_name = 'users'
 
     def get_short_name(self):
-        return self.first_name
+        return self.first_name if self.first_name is not None else self.email
 
     def get_full_name(self):
         return '{0} {1}'.format(self.first_name, self.last_name)
