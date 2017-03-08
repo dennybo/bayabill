531b0ebd5d33ab82152260126b374553679a2ce3
Iman Vafadarnezhad <iman.v846@gmail.com>
2017-03-08 19:21:56 +0330

    Added cart, setting and user to admin

--- billing/admin.py
+++ billing/admin.py
@@ -1,42 +1,57 @@
 from django.contrib import admin
+from django.contrib.sites.models import Site
 
-from .models import Language, Currency, Country, Store, Category, CategoryLang, Product, ProductLang, ProductPricing
+from account.models import User
+from .models import Language, Currency, Country, Store, Setting, Category, CategoryLang, Product, ProductLang, ProductPricing, Cart, CartItem
 
 
 class LanguageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'iso_code', 'title', 'is_active']
+    list_display = ['id', 'title', 'iso_code', 'is_active']
     list_display_links = ['iso_code', 'title']
     search_fields = ['iso_code', 'title']
 
 
 class CurrencyAdmin(admin.ModelAdmin):
-    list_display = ['id', 'iso_code', 'title',
+    list_display = ['id', 'title', 'iso_code',
                     'prefix', 'suffix', 'rate', 'is_active']
     list_display_links = ['iso_code', 'title']
     search_fields = ['iso_code', 'title']
 
 
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'iso_code', 'title',
+    list_display = ['id', 'title', 'iso_code',
                     'default_language', 'default_currency', 'is_active']
     list_display_links = ['iso_code', 'title']
     search_fields = ['iso_code', 'title']
 
 
 class StoreAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'default_language',
+    list_display = ['id', 'name', 'default_language',
                     'default_currency', 'default_country', 'is_default', 'is_active']
-    list_display_links = ['title']
-    search_fields = ['title']
+    list_display_links = ['name']
+    search_fields = ['name']
 
 
-class CategoryInline(admin.StackedInline):
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'value', 'store']
+    list_display_links = ['name', 'value']
+    search_fields = ['name', 'value']
+
+
+class CategoryLangInline(admin.StackedInline):
     model = CategoryLang
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'parent', 'store', 'is_active']
-    inlines = [CategoryInline]
+    list_display = ['id', 'title', 'parent', 'store', 'is_active']
+    list_display_links = ['title']
+    inlines = [CategoryLangInline]
+
+    def title(self, obj):
+        return CategoryLang.records.get(
+            category=obj,
+            language=obj.store.default_language,
+        ).title
 
 
 class ProductLangInline(admin.StackedInline):
@@ -48,13 +63,34 @@ class ProductPricingInline(admin.StackedInline):
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'category', 'store', 'is_active']
+    list_display = ['id', 'title', 'category', 'store', 'is_active']
+    list_display_links = ['title']
     inlines = [ProductLangInline, ProductPricingInline]
 
+    def title(self, obj):
+        return ProductLang.records.get(
+            product=obj,
+            language=obj.store.default_language,
+        ).title
 
+
+class CartItemInline(admin.StackedInline):
+    model = CartItem
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'currency', 'total_amount']
+    list_display_links = ['user']
+    search_fields = ['user']
+    inlines = [CartItemInline]
+
+admin.site.unregister(Site)
+admin.site.register(User)
 admin.site.register(Language, LanguageAdmin)
 admin.site.register(Currency, CurrencyAdmin)
 admin.site.register(Country, CountryAdmin)
 admin.site.register(Store, StoreAdmin)
+admin.site.register(Setting, SettingAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Cart, CartAdmin)
