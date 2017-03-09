d71e11b193be93aaad43c461b545a82ec48db870
Iman Vafadarnezhad <iman.v846@gmail.com>
2017-03-09 14:48:53 +0330

    Added category and product view templates

--- web/views.py
+++ web/views.py
@@ -2,7 +2,7 @@ from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from django.utils.translation import ugettext_lazy as _
 
-from billing.models import Category, CategoryLang, Language, Store, Product, ProductLang
+from billing.models import Category, CategoryLang, Language, Product, ProductLang, ProductPricing, Store
 
 
 def index(request):
@@ -13,12 +13,20 @@ def category(request, url_key):
     store = Store.records.get(name=request.site.name)
     try:
         language = Language.records.get(iso_code=request.LANGUAGE_CODE)
-        category = CategoryLang.records.get(
+        category = CategoryLang.records.select_related('category').get(
             category__store=store,
             language=language,
             url_key=url_key,
         )
-        return HttpResponse(category.title)
+        products = ProductLang.records.filter(
+            product__category=category.category,
+            language=language,
+        )
+        return render(request, 'web/category.html', {
+            'page_description': category.meta_description,
+            'category': category,
+            'products': products,
+        })
     except Language.DoesNotExist:
         raise Http404(_('Content is not available in this language.'))
     except CategoryLang.DoesNotExist:
@@ -29,12 +37,15 @@ def product(request, category_url_key, url_key):
     store = Store.records.get(name=request.site.name)
     try:
         language = Language.records.get(iso_code=request.LANGUAGE_CODE)
-        product = ProductLang.records.get(
+        product = ProductLang.records.select_related('product').get(
             product__store=store,
             language=language,
             url_key=url_key,
         )
-        return HttpResponse(product.title)
+        return render(request, 'web/product.html', {
+            'page_description': product.meta_description,
+            'product': product,
+        })
     except Language.DoesNotExist:
         raise Http404(_('Content is not available in this language.'))
     except ProductLang.DoesNotExist:
